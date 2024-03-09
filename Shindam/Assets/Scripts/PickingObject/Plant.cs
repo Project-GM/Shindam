a58d311fc663d4161cf55653f5448d75337d593f
leejung7308 <leekkzzzz@naver.com>
2024-03-09 11:46:07 +0900

    feat: plant.cs 추가
    채집물 데이터

---
+++ Shindam/Assets/Scripts/PickingObject/Plant.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 채집 오브젝트 스폰을 위한 채집물 데이터
+/// <summary>
+public class Plant : MonoBehaviour
+{
+
+}
