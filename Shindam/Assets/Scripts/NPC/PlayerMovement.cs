d6b5320e330388878030569eb894bc315ec3def8
jHyuk22 <jhlee4938@gmail.com>
2024-03-09 22:39:10 +0900

    feat: 패널 제작

---
+++ Shindam/Assets/Scripts/NPC/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveDirection;
+    public float moveSpeed;
+    Rigidbody2D playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = new Vector2(moveDirection * moveSpeed, playerRigidbody.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal");
+    }
+}
