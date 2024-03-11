51136c951322bdfd53735814384d30e8ec281234
leejung7308 <leekkzzzz@naver.com>
2024-03-12 03:07:35 +0900

    feat: 인벤토리 추가
    개수 제한: 30개
    가득찬 경우: 메시지 출력

--- Shindam/Assets/Scripts/Inventory/Inventory.cs
+++ Shindam/Assets/Scripts/Inventory/Inventory.cs
@@ -1,58 +1,120 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    //public List<TestItem> items;
+    public GameObject inventory;
     [SerializeField]
     private Transform slotParent;
     [SerializeField]
     private Slot[] slots;
+    [SerializeField]
+    CanvasGroup inventoryFullFloating;
+    
+    /// <summary>
+    /// 인벤토리 스크립트
+    /// 아이템 습득 및 분배 기능
+    /// </summary>
 
     private void Start()
     {
         slots = slotParent.GetComponentsInChildren<Slot>();
-        //SetItems();
     }
-    /*public void SetItems()
+    private void Update()
     {
-        int i = 0;
-        for (; i < items.Count && i < slots.Length; i++)
-            slots[i].item = items[i];
-        for (; i < slots.Length; i++)
-            slots[i].item = null;
-    }*/
-    public void AddItem(TestItem item)
+        TryOpenInventory(); // I키를 통해 열거나 닫거나
+        TryCloseInventory();// esc키를 통해 닫는다
+    }
+    public void TryOpenInventory()
     {
-        bool hasItem = false;
-        for (int i = 0; i < slots.Length; i++)
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            if (item == slots[i].item)
+            if(!inventory.activeSelf) OpenInventory();
+            else CloseInventory();
+        }
+    }
+    public void TryCloseInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("이상하네");
+            if (inventory.activeSelf) CloseInventory();
+        }
+    }
+    public void AddItem(Item item, int count = 1) //아이템 습득 함수 (아이템, 개수) 형식
+    {
+        bool hasItem = false; //이미 있는 아이템인지 확인
+        bool full = false;    //인벤토리가 가득찼는지 확인
+        for (int i = 0; i < slots.Length; i++) //이미 있는 아이템인지 확인하는 반복문
+        {
+            if (item == slots[i].item) //아이템이 인벤토리에 존재하면
             {
                 hasItem = true;
-                slots[i].item.itemCount++;
-                break;
+                slots[i].itemCount += count; //아이템 개수 더함
+                if (slots[i].itemCount > 30) //아이템 개수가 30개를 넘으면
+                {
+                    hasItem = false; //이미 하나의 슬롯이 가득 찼으므로 인벤토리에 아이템이 존재하지 않는다 표시(= 존재하지만 더 넣을 공간이 없다)
+                    count = slots[i].itemCount - 30; //초과된 개수만큼 다시 할당
+                    slots[i].itemCount = 30; //아이템 개수 30개로 설정
+                    slots[i].item = item; //슬롯 설정
+                    continue; //중복되는 아이템이 더 있는지 확인
+                }
+                slots[i].item = item; //슬롯 설정
+                break; //반복문 탈출
             }
         }
-        if (!hasItem)
+        if (!hasItem) //인벤토리에 같은 아이템이 존재하지 않으면
         {
             for (int i = 0; i < slots.Length; i++)
             {
-                if (slots[i].item == null) 
+                full = false;
+                if (slots[i].item == null) //빈 슬롯이 있으면
                 {
-                    slots[i].item = item;
-                    break;
+                    slots[i].itemCount += count; //아이템 개수 더함
+                    if (slots[i].itemCount > 30) //아이템 개수가 30개를 넘으면
+                    {
+                        count = slots[i].itemCount - 30; //초과된 개수만큼 다시 할당
+                        slots[i].itemCount = 30; //아이템 개수 30개로 설정
+                        slots[i].item = item; //슬롯 설정
+                        if(i == slots.Length - 1 && count > 0) //마지막 슬롯인데 아직 습득할 아이템이 존재하면
+                        {
+                            full = true; //가득찼다 표시
+                            break; //반복문 탈출
+                        }
+                        continue; //빈 슬롯이 더 있는지 확인
+                    }
+                    slots[i].item = item; //슬롯 설정
+                    break; //반복문 탈출
                 }
+                full = true; //아이템 중복, 빈 슬롯 전부 확인했는데 그 무엇도 안나왔을 경우 가득찼다 표시
             }
         }
+        if (full) StartCoroutine(EnableInventoryFullFloating());
     }
-    public void CloseInventory()
+    public void CloseInventory() //인벤토리 닫는 함수
     {
-        gameObject.SetActive(false);
+        inventory.SetActive(false);
     }
-    public void OpenInventory()
+    public void OpenInventory() //인벤토리 여는 함수
     {
-        gameObject.SetActive(true);
+        inventory.SetActive(true);
+    }
+    IEnumerator EnableInventoryFullFloating()
+    {
+        inventoryFullFloating.gameObject.SetActive(true);
+        while (inventoryFullFloating.alpha < 1)
+        {
+            inventoryFullFloating.alpha += Time.deltaTime * 1.5f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        while (inventoryFullFloating.alpha > 0)
+        {
+            inventoryFullFloating.alpha -= Time.deltaTime * 1.5f;
+            yield return null;
+        }
+        inventoryFullFloating.gameObject.SetActive(false) ;
     }
 }
--- Shindam/Assets/Scripts/Inventory/Slot.cs
+++ Shindam/Assets/Scripts/Inventory/Slot.cs
@@ -1,28 +1,34 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// 인벤토리 슬롯 스크립트
+/// 아이템 및 아이템 개수 관리
+/// </summary>
+
 public class Slot : MonoBehaviour
 {
-    [SerializeField] Image image;
-    [SerializeField] TMP_Text itemCountText;
-    private TestItem _item;
-    public TestItem item
+    [SerializeField] Image image; //아이템 이미지
+    [SerializeField] TMP_Text itemCountText; //아이템 개수 텍스트
+    private Item _item; //슬롯에 할당된 아이템
+    public int itemCount = 0; //아이템 개수
+    public Item item
     {
-        get { return _item; }
-        set
+        get { return _item; } //아이템 반환
+        set //슬롯 설정
         {
             _item = value;
-            if (_item != null)
+            if (_item != null) //아이템이 할당되면
             {
                 image.sprite = item.itemImage;
                 image.color = new Color(1, 1, 1, 1);
                 itemCountText.gameObject.SetActive(true);
-                itemCountText.text = item.itemCount.ToString();
+                itemCountText.text = itemCount.ToString();
             }
-            else
+            else //아이템이 할당 안되면
             {
                 image.color = new Color(1, 1, 1, 0);
                 itemCountText.gameObject.SetActive(false);
