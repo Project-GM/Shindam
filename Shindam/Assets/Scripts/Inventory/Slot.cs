2b828c2b6d6c57a02119dcbe5cf394c2326ad59e
leejung7308 <leekkzzzz@naver.com>
2024-03-13 20:08:38 +0900

    chore: TMP 삭제

--- Shindam/Assets/Scripts/Inventory/Slot.cs
+++ Shindam/Assets/Scripts/Inventory/Slot.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 /// <summary>
 /// 인벤토리 슬롯 스크립트
@@ -12,7 +11,7 @@ using TMPro;
 public class Slot : MonoBehaviour
 {
     [SerializeField] Image image; //아이템 이미지
-    [SerializeField] TMP_Text itemCountText; //아이템 개수 텍스트
+    [SerializeField] Text itemCountText; //아이템 개수 텍스트
     private Item _item; //슬롯에 할당된 아이템
     public int itemCount = 0; //아이템 개수
     public Item item
