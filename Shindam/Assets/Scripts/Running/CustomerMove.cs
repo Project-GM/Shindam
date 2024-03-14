fd87d8ca0a317082c797b42ce4a8965be3182816
jHyuk22 <jhlee4938@gmail.com>
2024-03-14 16:34:16 +0900

    feat: 손님 이동 스크립트 수정

--- Shindam/Assets/Scripts/Running/CustomerMove.cs
+++ Shindam/Assets/Scripts/Running/CustomerMove.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CustomerMove : MonoBehaviour
 {
     public float customerSpeed = 1f;
-    SpriteRenderer spriteRenderer;
+    public GameObject door;
 
+    SpriteRenderer spriteRenderer;
     private bool isFinish = false;
     private bool isSitted = false;
+    private Transform chair;
 
     void Start()
     {
@@ -32,16 +34,31 @@ public class CustomerMove : MonoBehaviour
     {
         transform.Translate(new Vector3(customerSpeed * Time.deltaTime, 0, 0));
         spriteRenderer.flipX = false;
+        if (Vector3.Distance(transform.position, chair.position) < 0.1f)
+        {
+            isSitted = true; //손님 의자에 앉음
+        }
+
     }
 
     void moveLeft()
     {
         transform.Translate(new Vector3(-1f * customerSpeed * Time.deltaTime, 0, 0));
         spriteRenderer.flipX = true;
+        if (Vector3.Distance(transform.position, door.transform.position) < 0.1f)
+        {
+            Destroy(gameObject); // 손님이 퇴장 지점에 도달하면 객체 파괴
+        }
     }
 
     void moveStop()
     {
+        Order();
+    }
+
+    void Order()
+    {
+        //일단 녹차만 주문할게요
 
     }
 }
